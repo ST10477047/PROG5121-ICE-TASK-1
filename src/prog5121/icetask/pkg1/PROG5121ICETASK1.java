0b6c08ea94f412640f384fa86baf1ce09f4d9261
ST10477047 <ST10477047@myemeris.edu.za>
2026-03-11 15:31:05 +0200

    Created Project and Main class

---
+++ src/prog5121/icetask/pkg1/PROG5121ICETASK1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prog5121.icetask.pkg1;
+
+/**
+ *
+ * @author deanb
+ */
+public class PROG5121ICETASK1 {
+   
+    public static void main(String[] args) {
+        
+        System.out.println("Campus Quick Serve Ordering App");
+      
+    }
+    
+}

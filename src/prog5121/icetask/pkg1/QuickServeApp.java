4a346a8c5f136d70d2d1bea703797e8532a953ce
ST10477047 <ST10477047@myemeris.edu.za>
2026-03-11 16:58:58 +0200

    Added Vat discount and changed some variables to private and more

--- src/prog5121/icetask/pkg1/QuickServeApp.java
+++ src/prog5121/icetask/pkg1/QuickServeApp.java
@@ -2,10 +2,11 @@ package prog5121.icetask.pkg1;
 import javax.swing.JOptionPane;
 
 class Order {
-    String item;
-    int quantity;
-    double price;
+   private String item;
+   private int quantity;
+   private double price;
     final double VAT_RATE = 0.15;
+    final double DISCOUNT_RATE = 0.10;
     
     public Order(String Item, int quantity, double price) {
         this.item= item;
@@ -14,16 +15,35 @@ class Order {
         
     }
     
+    public String getItem() {
+        return item;
+    }
+    
+    public int getQuantity(){
+        return quantity;
+    }
+    
+    public double getPrice(){
+        return price;
+    }
     public double getSubtotal(){
         return quantity * price;
     }
     
+    public double getDiscount() {
+        if (quantity > 3) {
+            return getSubtotal() * DISCOUNT_RATE;
+        } else {
+            return 0;
+        }
+    }
+        
     public double getVAT(){
-        return getSubtotal() * VAT_RATE;
+        return (getSubtotal() - getDiscount()) * VAT_RATE;
     }
     
     public double getTotal() {
-        return getSubtotal() + getVAT();
+        return (getSubtotal() - getDiscount()) + getVAT();
     }
 }
 
@@ -43,14 +63,15 @@ class Order {
         String receipt = "---- CAMPUS QUICKSERVE----\n"
                 +"Customer: " + name + "\n"
                 +"Student Number: " + studentNumber + "\n\n"
-                +"Item Ordered: " + order.item + "\n"
-                +"Quantity: " + order.quantity + "\n"
-                +"Price per Item: R" +String.format("%.2f", order.price) + "\n\n"
+                +"Item Ordered: " + order.getItem() + "\n"
+                +"Quantity: " + order.getQuantity() + "\n"
+                +"Price per Item: R" +String.format("%.2f", order.getPrice()) + "\n\n"
                 +"Subtotal: R" + String.format("%.2f", order.getSubtotal()) + "\n"
-                + "VAT (15%): R" + String.format("%.2f", order.getVAT()) + "\n"
-                + "Total: R" + String.format("%.2f", order.getTotal()) + "\n\n"
-                + "Thank you for your order!\n"
-                + "------------------------------";
+                +"Discount Applied: R" + String.format("%.2f", order.getDiscount()) + "\n"
+                +"VAT (15%): R" + String.format("%.2f", order.getVAT()) + "\n"
+                +"Total: R" + String.format("%.2f", order.getTotal()) + "\n\n"
+                +"Thank you for your order!\n"
+                +"----------------------------------";
                 
         System.out.println(receipt);
         JOptionPane.showMessageDialog(null, receipt);
